a3f2b2c4c1fdf3a457da3cc73e1f1d2d089c4d40
Jonathan Beck <jonathanbeck@gmail.com>
2018-02-12 16:12:00 -0800

    fixed a few bugs for logged out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+ 
+
+
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get 'signin', to: 'devise/sessions#new'
+    delete 'signout', to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   get 'feed', to: 'feed#show'
+
 
   resources :users, only: :show, param: :username do
     member do 
@@ -10,13 +22,7 @@ Rails.application.routes.draw do
   resources :items
   resources :problems
   resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-  	get 'signin', to: 'devise/sessions#new'
-  	delete 'signout', to: 'devise/sessions#destroy'
-  	get 'signup', to: 'devise/registrations#new'
-  end
+
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact-us', to: 'pages#contact-us'
